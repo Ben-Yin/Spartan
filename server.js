30ded6f99f58a2966f5bbddd7070cf9d0659f5ab
ChaosInOrder <qinxuliang2015@gmail.com>
2017-04-01 22:45:07 -0400

    Add security

--- server.js
+++ server.js
@@ -3,9 +3,16 @@ var app = express();
 var bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+var cookieParser=require('cookie-parser');
+var session=require('express-session');
+var passport=require('passport');
 
 // configure a public directory to host static content
 app.use(express.static(__dirname + '/public'));
+app.use(cookieParser());
+app.use(session({ secret: process.env.SESSION_SECRET }));
+app.use(passport.initialize());
+app.use(passport.session());
 
 var models = require("./models/models.server")();
 require("./services/user.service.server")(app, models);
